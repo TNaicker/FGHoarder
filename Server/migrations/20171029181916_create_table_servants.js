c9222980f20b3097ce8b28dc28ab549528387311
Tegan Naicker <Tegan.Naicker@gmail.com>
2017-10-29 22:49:08 -0400

    Add prototype table schemas

--- Server/migrations/20171029181916_create_table_servants.js
+++ Server/migrations/20171029181916_create_table_servants.js
@@ -1,8 +1,18 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('servants', (table) => {
+    table.increments();
+    table.string('servant_name', 50).notNullable();
+    table.string('class', 20).notNullable();
+    table.integer('id_number');
+    table.integer('cost').notNullable();
+    table.string('rarity').notNullable();
+    table.string('atk_lvl_100').notNullable();
+    table.string('hp_lvl_100').notNullable();
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('servants');
 };
